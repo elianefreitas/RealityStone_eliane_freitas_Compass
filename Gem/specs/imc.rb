e98b1f2824b74c5cd8efb960ea7454944be2be4c
elianefreitas <elianesfreitas.freitas@gmail.com>
2022-09-01 20:18:25 -0300

    teste

--- Gem/specs/imc.rb
+++ Gem/specs/imc.rb
@@ -8,7 +8,7 @@ end
 describe('Calculo do IMC') do
    
    it('deve calcular peso e altura corretos') do
-     peso = 60
+      peso = 60
       altura = 1,70
       expect(imc(peso, altura)).to eq 20,7
   end

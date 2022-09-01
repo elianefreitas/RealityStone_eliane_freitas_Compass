c3d59774ead46fedf6d678e115c1281455d01a61
elianefreitas <elianesfreitas.freitas@gmail.com>
2022-09-01 20:08:21 -0300

    teste

--- Gem/specs/imc.rb
+++ Gem/specs/imc.rb
@@ -1,14 +1,15 @@
 require 'rspec'
 
 def imc(peso, altura)
-  return peso / (altura * altura)
+  imc_calculado = peso / (altura * altura)
+  return imc_calculado.round(1)
 end
 
 describe('Calculo do IMC') do
    
-    it('deve calcular peso e altura corretos') do
-      peso = 60
+   it('deve calcular peso e altura corretos') do
+     peso = 60
       altura = 1,70
-     expect(imc(peso, altura)).to eq 20,7
-   end
+      expect(imc(peso, altura)).to eq 20,7
+  end
 end

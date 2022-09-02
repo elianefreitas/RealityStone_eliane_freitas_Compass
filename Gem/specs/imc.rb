e785ea192b677a3ece514cdc930ccaad9f2ab4b4
elianefreitas <elianesfreitas.freitas@gmail.com>
2022-09-01 22:42:11 -0300

    atualizando readme

--- Gem/specs/imc.rb
+++ Gem/specs/imc.rb
@@ -1,16 +1,19 @@
-require 'rspec'
+require "rspec"
+require_relative "../src/imc.rb"
 
-def imc(peso, altura)
-  imc_calculado = peso/(altura * altura)
-  return imc_calculado.round(1)
-end
 
-describe('Calculo do IMC') do
-   it('deve calcular peso e altura corretos') do
-      peso = 60
-      altura = 1,70
-      expect(imc(peso,altura)).to eq 20.8
-  
-  
-  end
+describe("Calculo do IMC") do
+
+    it("Deve calcular com peso e altura corretos") do
+        peso = 60
+        altura = 1.70
+        expect(imc(peso,altura)).to eq 20.8
+    end
+
+    it("Não deve calcular o IMC para altura = 0") do 
+        peso = 60
+        altura = 0
+        expect(imc(peso,altura)).to include "Não foi possivel calcular o IMC para altura = 0"
+    end
+
 end

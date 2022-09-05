188ae245bed4fe0c49a948543c158829b2cf969b
elianefreitas <elianesfreitas.freitas@gmail.com>
2022-09-04 22:46:24 -0300

    atualizando

--- Gem/specs/calculadora_spec.rb
+++ Gem/specs/calculadora_spec.rb
@@ -3,38 +3,77 @@ require_relative '../src/calculadora'
 
 describe('calculadora') do
     calculadora = Calculadora.new
-
-  it('deve somar um numero qualquer com 0') do
-    expect(calculadora.soma(7, 0)).to eq 7
+           
+    
+           ###soma_correta###
+  it('deve somar um numeros iguais') do
+    expect(calculadora.soma(18, 18)).to eq 36
   end
   
-  it('deve somar um numero qualquer com 0') do
-    expect(calculadora.soma(-9, -11)).to eq -19
+           ###soma_errada###
+  it('deve somar um numero qualquer com negativo') do
+    expect(calculadora.soma(-9, 10)).to eq -19
   end
   
+           ###soma_correta###
   it('deve subtrair dois inteiros positivos') do
-    expect(calculadora.subtrair(8, 2)).to eq 6
+    expect(calculadora.subtrair(55, 15)).to eq 40
   end
 
-  it('deve subtrair dois inteiros positivos') do
-    expect(calculadora.subtrair(8, 5)).to eq 6
+           ###soma_errada###
+  it('Deve subtrair números float positivos com resultado float') do
+    expect(calculadora.subtrair(8.5, 5.5)).to eq 6.9
   end
 
+
+           ###soma_correta###
   it('deve dividir dois inteiros positivos') do
     expect(calculadora.dividir(9, 3)).to eq 3
   end
 
+           ###soma_errada###
   it('deve dividir dois inteiros positivos') do
-    expect(calculadora.dividir(9, 3)).to eq 2
+    expect(calculadora.dividir(33, 3)).to eq 13
   end
 
+            ###soma_correta###
   it('deve multiplicar dois inteiros negativos') do
     expect(calculadora.multiplicar(5, 4)).to eq 20
   end
 
+           ###soma_errada###
   it('deve multiplicar dois inteiros positivos') do
     expect(calculadora.multiplicar(-5, 4)).to eq 20
   end
 
+           ###soma_correta###
+  it("Deve potencializar qualquer número por 3")do
+    expect(calculadora.potencial(6, 3)).to eq 216
+  end
+
+           ###soma_errada###
+  it("deve potencializar qualquer número por 3")do
+    expect(calculadora.potencial(7, 2)).to eq 52
+  end
+   
+           ###soma_correta###
+  it('deve mostrar a raiz quadrada de um número') do
+        expect(calculadora.quad(9)).to eq 3
+  end
+
+           ###soma_errada###
+  it('deve mostrar a raiz quadrada de um número') do
+    expect(calculadora.quad(15)).to eq 6
+  end
+
+           ###soma_errada###
+  it('deve calcular a porcentagem de um numero inteiro') do
+    expect(calculadora.porcentagem(50, 60)).to eq 90
+  end
+
+           ###soma_correta###
+  it('deve calcular a porcentagem de um numero inteiro') do
+    expect(calculadora.porcentagem(80, 200)).to eq 160
+  end
 
 end
--- Gem/src/calculadora.rb
+++ Gem/src/calculadora.rb
@@ -1,20 +1,36 @@
 class Calculadora
   
-  def soma(a, b)
-    a + b
+  def soma(num_x, num_y)
+    num_x + num_y
   end
 
-  def subtrair(a, b)
-    a - b
+  def subtrair(num_x, num_y)
+    num_x - num_y
   end
 
-  def dividir(a, b)
-    a / b
+  def dividir(num_x, num_y)
+    num_x / num_y
   end
 
-  def multiplicar(a, b)
-    a * b
+  def multiplicar(num_x, num_y)
+    num_x * num_y
   end
 
+  def potencial(num_x, num_y)
+    if a or b < 0
+     return (num_x**num_y.to_f).round(6)
+    end
+    (num_x ** num_y)
+  end
+
+  def quad(num_x)
+    return "null" if a < 1
+    resultado = Math.sqrt(num_x)
+    resultado.round(1)         
+  end
+
+  def porcentagem(num_x, num_y)
+    num_x * num_y / 100
+  end
 
 end  
